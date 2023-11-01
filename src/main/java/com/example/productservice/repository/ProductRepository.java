ec9fb07223718cdc66cfe644a78f994a6ead1654
hamzaacode <amir.hamza.code@gmail.com>
2023-11-01 12:23:31 +0600

    Initial Commit

---
+++ src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.productservice.repository;
+
+import com.example.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
